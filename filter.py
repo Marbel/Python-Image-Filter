305b2ea03e907c8e0aa6d52b4afdbb7e30c91db9
Marbel <das.marbel@googlemail.com>
2017-08-27 10:55:39 +0200

    Test Convert RGB to Gray

--- filter.py
+++ filter.py
@@ -9,8 +9,12 @@ img = cv2.imread("test.jpg")
 
 size = img.shape
 
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 cv2.imshow('test',img)
+cv2.imshow('test1',gray)
+
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
