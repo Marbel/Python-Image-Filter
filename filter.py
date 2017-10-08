31eefd5963e54a08217f397264a781a05fca12a0
Marbel <das.marbel@googlemail.com>
2017-10-08 13:00:25 +0200

    Add filter, grayscale img and a saving mechanism

--- filter.py
+++ filter.py
@@ -6,34 +6,55 @@ def dummy(val):
 
 identity_kernel = np.array([[0,0,0],[0,1,0],[0,0,0]])
 sharpen_kernel = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
-gaussian_kernel = np.array([[1,2,1],[2,4,2],[1,2,1]], np.float32) / 16
+# use cv2 kernel
+gaussian_kernel1 = cv2.getGaussianKernel(3,0)
+gaussian_kernel2 = cv2.getGaussianKernel(5,0)
+box_kernel = np.array([[1,1,1],[1,1,1],[1,1,1]], np.float32) / 9
 
-kernels = [identity_kernel,sharpen_kernel,gaussian_kernel]
+kernels = [identity_kernel,sharpen_kernel,gaussian_kernel1,gaussian_kernel2,box_kernel]
 
 windowName = 'app'
 color_original = cv2.imread("test.jpg")
 color_modified = color_original.copy()
+gray_original = cv2.cvtColor(color_original,cv2.COLOR_BGR2GRAY)
+gray_modified = gray_original.copy()
 
 cv2.namedWindow(windowName)
 cv2.createTrackbar('contrast',windowName,1,100,dummy)
 cv2.createTrackbar('brightness',windowName,50,100,dummy)
 cv2.createTrackbar('filter',windowName,0,len(kernels)-1,dummy)
-cv2.createTrackbar('grayScale',windowName,0,1,dummy)
+cv2.createTrackbar('grayscale',windowName,0,1,dummy)
+
+savecounter = 1
 
 while True:
-    cv2.imshow(windowName,color_modified)
+    grayscale = cv2.getTrackbarPos('grayscale',windowName)
+    if grayscale == 0:
+        cv2.imshow(windowName,color_modified)
+    else:
+        cv2.imshow(windowName,gray_modified)
+
     k = cv2.waitKey(1) & 0xFF
     if k == ord('q'):
         break
+    elif k == ord('s'):
+        if grayscale == 0:
+            cv2.imwrite('output%d.png' % savecounter,color_modified)
+        else:
+            cv2.imwrite('output%d.png' % savecounter,gray_modified)
+        savecounter = savecounter + 1
 
     contrast = cv2.getTrackbarPos('contrast',windowName)
     brightness = cv2.getTrackbarPos('brightness',windowName)
     kernel = cv2.getTrackbarPos('filter',windowName)
 
+
     color_modified = cv2.filter2D(color_original,-1,kernels[kernel])
+    gray_modified = cv2.filter2D(gray_original,-1,kernels[kernel])
 
     # Apply modifications to picture
     color_modified = cv2.addWeighted(color_modified,contrast,np.zeros(color_original.shape,dtype=color_original.dtype),0,brightness-50)
+    gray_modified = cv2.addWeighted(gray_modified,contrast,np.zeros(gray_original.shape,dtype=gray_original.dtype),0,brightness-50)
 
 cv2.destroyAllWindows()
 
